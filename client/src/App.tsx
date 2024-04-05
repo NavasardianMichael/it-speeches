e7abe67d4173488c34dddb9801fdd76234460969
NavasardianMichael <navasardianmichael@gmail.com>
2024-04-05 17:44:43 +0400

    routers added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [conferences, setConferences] = useState([])
 
+  useEffect(() => {
+    if(conferences.length) return
+    const getConferences = async () => {
+      const response = await fetch('http://localhost:5000/conference')
+      const result = await response.json()  
+      setConferences(result);
+    }
+    getConferences()
+  }, [conferences])
+
+  const saveConference = () => {
+    fetch('http://localhost:5000/conference', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: "Web Technologies",
+        location: "Street:  Rua C 885 City:  Colatina State/province/area:   Espírito Santo Phone number:  (27) 6837-6334 Zip code:  29701-140 Country calling code:  +55 Country:  Brazil",
+        dates: {
+          start: "Tue Dec 19 2023",
+          end: "Friday Dec 22 2023"
+        },
+        speechIds: []
+      })
+    })  
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={saveConference}>Send</button>
+      {
+        conferences.map(conference => {
+          return <p key={conference._id}>{conference.title}</p>
+        })
+      }
     </>
   )
 }
